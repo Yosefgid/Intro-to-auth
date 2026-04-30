df74b5186539fb41de529a4587e19879e190493e
Yosefgid <jossykasshun@yahoo.com>
2026-04-30 12:03:26 +0100

    feat: add an event class

---
+++ ConferenceManager/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿namespace ConferenceManager.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateOnly Date { get; set; }
+
+        public string Venue { get; set; }
+        public string Description {get; set; }
+
+        public string Category { get; set; }
+
+    }
+}
