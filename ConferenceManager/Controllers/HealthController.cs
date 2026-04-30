2f55b23f80f36002346003394f50958af20e7515
Yosefgid <jossykasshun@yahoo.com>
2026-04-30 11:31:27 +0100

    Add project files.

---
+++ ConferenceManager/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConferenceManager.Controllers
+{
+    public class HealthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
