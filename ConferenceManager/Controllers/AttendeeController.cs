dffaa8770b4153f0786b66ae08dd93cd0b57b202
Yosefgid <jossykasshun@yahoo.com>
2026-05-01 15:15:31 +0100

    feat: add service and controller for attendee

---
+++ ConferenceManager/Controllers/AttendeeController.cs
@@ -0,0 +1,65 @@
+﻿using ConferenceManager.Models;
+using ConferenceManager.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConferenceManager.Controllers
+{
+    [ApiController]
+    [Route("/attendees")]
+    public class AttendeeController : Controller
+    {
+        private readonly IAttendeeServices _attendeeService;
+        private readonly IEventService _eventService;
+
+        public AttendeeController(IAttendeeServices attendeeServices, IEventService eventService)
+        {
+            _attendeeService = attendeeServices;
+            _eventService = eventService;
+        }
+
+        //[Authorize(Roles = "Admin")]
+        [HttpGet]
+        public IActionResult GetAllAttendees()
+        {
+            return Ok(_attendeeService.FetchAllAttendees());
+
+        }
+        //[Authorize]
+        [HttpGet("{attendeeId")]
+        public IActionResult GetAllAttendeeById(int attendeeId)
+        {
+            return Ok(_attendeeService.FetchAttendeeById(attendeeId));
+        }
+
+        //[Authorize(Roles = "Admin"]
+        [HttpPost]
+        public IActionResult AddAttendee(Attendee newAttendee)
+        {
+            var created = _attendeeService.AddAttendee(newAttendee);
+            return CreatedAtAction(nameof(GetAllAttendeeById), new { id = newAttendee.AttendeeId }, newAttendee);
+        }
+
+
+        //[Authorize(Roles = "Admin"]
+        [HttpPut("{attendeeId}")] 
+        public IActionResult UpdateAttendee(int attendeeId, Attendee updateAttendee)
+        {
+            var result = _attendeeService.UpdateAttendee(attendeeId, updateAttendee);
+            return Ok(result);
+        }
+
+
+
+        //[Authorize(Roles = "Admin"]
+        [HttpDelete("{attendeeId}")]
+        public IActionResult DeleteAttendee(int attendeeId)
+        {
+            var result = _attendeeService.DeleteAttendee(attendeeId);
+            return NoContent();
+        }
+
+
+
+    }
+}
--- ConferenceManager/Services/AttendeeService.cs
+++ ConferenceManager/Services/AttendeeService.cs
@@ -1,10 +1,48 @@
 ﻿using ConferenceManager.Models;
+using ConferenceManager.Repositories;
 
 namespace ConferenceManager.Services
 {
-    public class AttendeeService
+    public interface IAttendeeServices
     {
+        public List<Attendee> FetchAllAttendees();
 
-        public List<Attendees>
+        public Attendee FetchAttendeeById(int attendeeId);
+        public Attendee AddAttendee(Attendee attendee);
+
+        public Attendee UpdateAttendee(int attendeeId, Attendee updateAttendee);
+        public bool DeleteAttendee(int attendeeId);
+
+    }
+    public class AttendeeService : IAttendeeServices
+    {
+        private readonly IAttendeeRepo _attendeeRepo;
+        public AttendeeService(IAttendeeRepo attendeeRepo)
+        {
+            _attendeeRepo = attendeeRepo;
+        }
+
+        public List<Attendee> FetchAllAttendees()
+        {
+            return _attendeeRepo.FetchAttendees();
+            
+        }
+        public Attendee FetchAttendeeById(int attendeeId)
+        {
+            return _attendeeRepo.FetchAttendeeById(attendeeId);
+        }
+        public Attendee AddAttendee(Attendee attendee)
+        {
+            return _attendeeRepo.AddAttendee(attendee);
+        }
+
+        public Attendee UpdateAttendee(int attendeeId, Attendee updateAttendee)
+        {
+            return _attendeeRepo.UpdateAttendee(attendeeId, updateAttendee);
+        }
+        public bool DeleteAttendee(int attendeeId)
+        {
+            return _attendeeRepo.DeleteAttendee(attendeeId);
+        }
     }
 }
