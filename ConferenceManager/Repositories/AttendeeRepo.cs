b2210328e5d522d1465211efc64db4959eb8c705
Konkz7 <amara03@live.co.uk>
2026-05-01 14:01:31 +0100

    weird repo merge

--- ConferenceManager/Repositories/AttendeeRepo.cs
+++ ConferenceManager/Repositories/AttendeeRepo.cs
@@ -1,19 +1,78 @@
-﻿using ConferenceManager.Models;
+﻿
+using ConferenceManager.Models;
+using System.Text.Json;
 
 namespace ConferenceManager.Repositories
 {
     public interface IAttendeeRepo
     {
-        List<Attendee> FetchAttendees(int eventId);
+        public List<Attendee> FetchAttendees();
 
-        Attendee FetchAttendeeById(int eventId ,int attendeeId);
+        public Attendee FetchAttendeeById(int attendeeId);
+        public Attendee AddAttendee(Attendee attendee);
+
+        public Attendee UpdateAttendee(int attendeeId, Attendee updateAttendee);
+        public bool DeleteAttendee(int attendeeId);
 
     }
-    public class AttendeeRepo
+    public class AttendeeRepo : IAttendeeRepo
     {
-        private readonly List<Attendee> _attendees;
+        private readonly string filePath = "/AttendeeData.json";
+        JsonSerializerOptions options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true, WriteIndented = true };
 
 
-        
+        public List<Attendee> FetchAttendees()
+        {
+            var json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<List<Attendee>>(json);
+        }
+
+        public Attendee FetchAttendeeById(int attendeeId)
+        {
+            return FetchAttendees().FirstOrDefault(a => a.AttendeeId == attendeeId);
+        }
+
+        public Attendee AddAttendee(Attendee attendee)
+        {
+            var attendees = FetchAttendees();
+            attendee.AttendeeId = attendees.Count > 0 ? attendees.Max(a => a.AttendeeId) + 1 : 1;
+            attendees.Add(attendee);
+            var result = JsonSerializer.Serialize(attendees, options);
+            File.WriteAllText(filePath, result);
+            return attendee;
+
+        }
+
+        public Attendee UpdateAttendee(int attendeeId, Attendee updateAttendee)
+        {
+            var attendees = FetchAttendees();
+            var attending = attendees.FirstOrDefault(a => a.AttendeeId == attendeeId);
+            //attending.UserId = updateAttendee.UserId; POTENTIALLY CAN BE USED
+            attending.EventId = updateAttendee.EventId;
+            var result = JsonSerializer.Serialize(attendees, options);
+            File.WriteAllText(filePath, result);
+            return attending;
+        }
+        public bool DeleteAttendee(int attendeeId)
+        {
+            var attendees = FetchAttendees();
+            var attendee = attendees.FirstOrDefault(a => a.AttendeeId == attendeeId);
+            attendees.Remove(attendee);
+            var result = JsonSerializer.Serialize(attendees, options);
+            File.WriteAllText(filePath, result);
+            return true;
+        }
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
