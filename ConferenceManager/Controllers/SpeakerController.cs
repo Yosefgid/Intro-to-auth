b8009ec92a28b333cc8e13761188f960eb0bfed6
Konkz7 <amara03@live.co.uk>
2026-05-01 14:50:01 +0100

    Speaker service and controller implemennted true

---
+++ ConferenceManager/Controllers/SpeakerController.cs
@@ -0,0 +1,58 @@
+﻿using ConferenceManager.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConferenceManager.Controllers
+{
+
+    [ApiController]
+    [Route("/speaker")]
+    public class SpeakerController : ControllerBase
+    {
+        private readonly ISpeakerService _speakerService;
+
+        public SpeakerController(ISpeakerService speakerService)
+        {
+            _speakerService = speakerService;   
+        }
+
+        [HttpGet("{eventId}")]
+        public IActionResult GetSpeakersByEventId(int eventId)
+        {
+            return Ok(_speakerService.FetchSpeakersbyEventId(eventId));
+        }
+
+        //[Authorize(Roles = "Admin")]
+        [HttpPost("{eventId}")]
+        public IActionResult AddSpeakerByEventId(int eventId)
+        {
+            var speaker = _speakerService.AddSpeakerToEvent(eventId);
+
+            return CreatedAtAction(nameof(GetSpeakersByEventId), new { Id = speaker.SpeakersId }, speaker);
+        }
+
+        //[Authorize(Roles = "Admin")]
+        [HttpDelete("{eventId}")]
+        public IActionResult AddSpeakerByEventId(int eventId , int speakerId )
+        {
+
+            _speakerService.RemoveSpeakerAtEvent(eventId, speakerId);
+            return NoContent();
+        }
+
+        //[Authorize(Roles = "Admin")]
+        [HttpPut("{eventId}")]
+        public IActionResult UpdateSpeakerinEvent(int eventId, int speakerId)
+        {
+            _speakerService.UpdateSpeakerinEvent(eventId, speakerId);
+            return Ok(); 
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ ConferenceManager/Services/SpeakerService.cs
@@ -0,0 +1,51 @@
+﻿using ConferenceManager.Models;
+using ConferenceManager.Repositories;
+
+namespace ConferenceManager.Services
+{
+    public interface ISpeakerService
+    {
+        List<Speaker> FetchSpeakersbyEventId(int eventId);
+
+        Speaker AddSpeakerToEvent(int eventId);
+
+        void RemoveSpeakerAtEvent(int eventId, int speakerId);
+
+        Speaker UpdateSpeakerinEvent(int newEventId, int speakerId);
+    } 
+    public class SpeakerService : ISpeakerService
+    {
+
+        private readonly ISpeakerRepo _speakerRepo;
+
+        public SpeakerService(ISpeakerRepo speakerRepo)
+        {
+            _speakerRepo = speakerRepo;   
+        }
+
+
+        public List<Speaker> FetchSpeakersbyEventId(int eventId)
+        {
+            return _speakerRepo.FetchSpeakersbyEventId(eventId);
+        }
+
+        public Speaker AddSpeakerToEvent(int eventId)
+        {
+            return _speakerRepo.AddSpeakerToEvent(eventId);
+        }
+
+
+        public void RemoveSpeakerAtEvent(int eventId, int speakerId)
+        {
+            _speakerRepo.RemoveSpeakerAtEvent(eventId, speakerId);
+        }
+
+
+        public Speaker UpdateSpeakerinEvent(int newEventId, int speakerId)
+        {
+            return _speakerRepo.UpdateSpeakerinEvent(newEventId, speakerId); 
+        }
+
+       
+    }
+}
